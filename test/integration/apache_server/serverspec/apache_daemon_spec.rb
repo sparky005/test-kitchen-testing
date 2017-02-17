e35097203607b7a1378e19fa9b2a351056492db1
Adil Sadik <sparky.005@gmail.com>
2017-02-17 11:06:02 -0500

    Add some serverspec tests

--- test/integration/apache_server/serverspec/apache_daemon_spec.rb
+++ test/integration/apache_server/serverspec/apache_daemon_spec.rb
@@ -3,6 +3,7 @@ require 'serverspec'
 # required by serverspec
 set :backend, :exec
 
+# test apache server itself
 describe "Apache Daemon" do
 
     it "is listening on port 80" do
@@ -10,7 +11,16 @@ describe "Apache Daemon" do
     end
 
     it "has running instance of apache" do
-        expect(service("apache")).to be_running
+        expect(service("apache2")).to be_running
     end
 
 end
+
+describe file('/var/www/example') do
+    it { should be_directory }
+end
+
+describe file('/var/www/example/index.html') do
+    it { should contain 'Testing the test kitchen' }
+end
+
